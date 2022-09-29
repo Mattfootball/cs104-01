2e94414099edd504cd1caeb2b0f8393a4575b818
Mattfootball <114018398+Mattfootball@users.noreply.github.com>
2022-09-29 13:32:43 -0400

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,17 @@
+average = 0
+total = 0
+howmany = 0
+howmany = int(input("howmany test scores would you liked average"))
+testscore = int(input("testscore"))
+howmany = 3
+tescore= 60
+
+
+                      
+
+
+              
+
+                   
+
+                    
