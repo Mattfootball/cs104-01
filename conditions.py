552e2f61ec4d0775ff23ae3f1800b813fbc9c3d5
Mattfootball <114018398+Mattfootball@users.noreply.github.com>
2022-10-03 16:16:11 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,8 @@
+temperature = input("please enter the current temperature:")
+print("please enter the current temperature" + temperature)
+if temperature > 90:
+    print("wear shorts")
+
+    
+
+    
